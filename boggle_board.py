ffec7dd6de9019fce51fadb2c3c7e94c40149cf8
Jonathan Young <jonyoungg@gmail.com>
2018-10-16 15:33:54 -0500

    Update boggle_board.py

--- boggle_board.py
+++ boggle_board.py
@@ -4,11 +4,3 @@ class BoggleBoard:
   
 
   def shake(self):
-  
-
-  # Defining to_s on an object controls how the object is
-  # represented as a string, e.g., when you pass it to puts
-  def to_s(self):
-    "Why would there be a built in ruby method here? Can it be overidden. Would that be useful?"
-  
-
