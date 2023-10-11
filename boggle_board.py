0950a93eb38a02514a218e8b2ca2d9cdaa89b162
Derek Marion <derek.j.marion@gmail.com>
2023-10-11 09:23:20 -0700

    Create BoggleBoard class with core shake method

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,24 @@
-class BoggleBoard:
-  
-  def __init__(self):
-  
+import string, random
 
+class BoggleBoard:
+  n = 4
+
+  def __init__(self):
+    self._board = ["____", "____", "____", "____"]
+    self.get_board
+  
+  @property
+  def get_board(self):
+    for group in self._board:
+      print(f"{group}\n")
+
+  
   def shake(self):
+    for idx in range(0, len(self._board)):
+      rando = ''.join(random.choices(string.ascii_uppercase, k=4))
+      self._board[idx] = rando
+    self.get_board
+
+new_board = BoggleBoard()
+new_board.shake()
+new_board.shake()
