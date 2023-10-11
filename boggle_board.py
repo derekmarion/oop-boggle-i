859ad1c3db8f075f80efc6b0b26a8ddf94eca45f
Derek Marion <derek.j.marion@gmail.com>
2023-10-11 11:54:07 -0700

    Added 16 spaces of 6 random letters to class functions

--- boggle_board.py
+++ boggle_board.py
@@ -1,11 +1,10 @@
 import string, random
 
 class BoggleBoard:
-  n = 4
-
-  def __init__(self):
-    self._board = ["____", "____", "____", "____"]
+  def __init__(self, dice):
+    self._board = ["______" for num in range(0, dice)]
     self.get_board
+    self._dice = dice
   
   @property
   def get_board(self):
@@ -15,10 +14,9 @@ class BoggleBoard:
   
   def shake(self):
     for idx in range(0, len(self._board)):
-      rando = ''.join(random.choices(string.ascii_uppercase, k=4))
+      rando = ''.join(random.choices(string.ascii_uppercase, k=6))
       self._board[idx] = rando
     self.get_board
 
-new_board = BoggleBoard()
-new_board.shake()
+new_board = BoggleBoard(16)
 new_board.shake()
